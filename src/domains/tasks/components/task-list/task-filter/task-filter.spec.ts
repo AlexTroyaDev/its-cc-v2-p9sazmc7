59da5ac831c10327b52c23be72638f0ffc14fc77
Alex Troya <alextroya@outlook.es>
2026-04-07 18:52:00 +0200

    Add taskFilter component and test

---
+++ src/domains/tasks/components/task-list/task-filter/task-filter.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TaskFilter } from './task-filter';
+
+describe('TaskFilter', () => {
+  let component: TaskFilter;
+  let fixture: ComponentFixture<TaskFilter>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TaskFilter]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TaskFilter);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('display filtering options', () => {
+    const filteringOptions = fixture.nativeElement.querySelectorAll('button');
+    expect(filteringOptions.length).toBe(3);
+    expect(filteringOptions[0].textContent).toBe('All');
+    expect(filteringOptions[1].textContent).toBe('Pending');
+    expect(filteringOptions[2].textContent).toBe('Completed');
+  })
+});
